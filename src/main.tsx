6f33ade9c1028a010b6d1eea018c425fc95e04b5
rongquan1 <rongquan.low@gmail.com>
2025-05-30 13:02:11 +0800

    feat: template setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { GalleryMainPage } from './screens/GalleryMainPage';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GalleryMainPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwind from "tailwindcss";
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
@@ -7,5 +8,10 @@ export default defineConfig({
   server: {
     open: true,
     port: 3000,
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind()],
+    },
   },
 });
