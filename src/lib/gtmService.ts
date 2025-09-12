e0996b1f3ca191c731f63d91a7f1374a0de2d0d6
moiz-sgtradex <moiz.shaikh@sgtradextech.com>
2025-09-12 14:44:04 +0530

    feat: type import update

--- src/lib/gtmService.ts
+++ src/lib/gtmService.ts
@@ -1,5 +1,5 @@
 import TagManager from 'react-gtm-module';
-import { GADataLayerEvent } from 'types/types';
+import { GADataLayerEvent } from '../types/types';
 
 // Initialize GTM immediately when this module is imported
 const gtmId = import.meta.env.VITE_GTM_ID;
